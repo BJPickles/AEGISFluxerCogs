2aef3ddf9c9287fb47c4b53354a605fbb77dcac1
Five <48354382+BJPickles@users.noreply.github.com>
2026-07-20 14:28:30 +0100

    Update __init__.py

--- fluxsuggestions/__init__.py
+++ fluxsuggestions/__init__.py
@@ -1,1 +1,10 @@
-TBA
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
+
+from .fluxsuggestions import FluxSuggestions
+
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
+
+
+async def setup(bot: Red) -> None:
+    await bot.add_cog(FluxSuggestions(bot))
