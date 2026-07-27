21749c668393f549b452dbcc8cdfdbd16685de7a
Five <48354382+BJPickles@users.noreply.github.com>
2026-07-27 21:27:42 +0100

    Update __init__.py

--- fluxquestions/__init__.py
+++ fluxquestions/__init__.py
@@ -1,1 +1,11 @@
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
 
+from .fluxquestions import FluxQuestions
+
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
+
+
+async def setup(bot: Red) -> None:
+    await bot.add_cog(FluxQuestions(b
+                                    ot))
